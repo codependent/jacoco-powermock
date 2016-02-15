02879603c075bd7b71e625b001048365f0738397
Jose Antonio Iñigo Garrido <JINGA4X@suministrador.local>
2016-02-15 10:25:50 +0100

    changes

--- src/test/java/com/codependent/jacoco/UtilTest.java
+++ src/test/java/com/codependent/jacoco/UtilTest.java
@@ -4,15 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareOnlyThisForTest;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import junit.framework.Assert;
 
 @RunWith(PowerMockRunner.class)
-@PrepareOnlyThisForTest(Util.class)
-@PowerMockIgnore("org.jacoco.agent.rt.*")
+@PrepareForTest({Util.class})
 public class UtilTest {
 
     @Test
